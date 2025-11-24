edc2cff7916614b8eca40f278b534c888b098738
ZeroHour <2719850558@qq.com>
2025-11-25 07:13:32 +0800

    waline

--- src/site.config.ts
+++ src/site.config.ts
@@ -161,7 +161,7 @@ export const integ: IntegrationUserConfig = {
   waline: {
     enable: true,
     // Server service link
-    server: 'https://astro-theme-pure-waline.arthals.ink/',
+    server: 'https://waline-virid-phi.vercel.app/',
     // Refer https://waline.js.org/en/guide/features/emoji.html
     emoji: ['bmoji', 'weibo'],
     // Refer https://waline.js.org/en/reference/client/props.html
